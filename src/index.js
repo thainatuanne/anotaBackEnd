8771c64da4b21a7ef59f7e5325db3eb48d020ac1
thainatuanne <thaina-tuanne@hotmail.com>
2025-01-22 18:00:11 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -5,32 +5,15 @@ import { messagesRouter } from "./routes/messages.js";
 
 const app = express();
 
-const allowedOrigins = ["https://anota-fron-end.vercel.app"];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
-
-app.use((err, req, res, next) => {
-  if (err.name === "CorsError") {
-    console.error("Erro de CORS:", err.message);
-    res.status(500).json({ message: "Erro de configuração CORS." });
-  } else {
-    next(err);
-  }
-});
 
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Bem vindo à aplicação" });
+  res.status(200).json({
+    message: "Bem vindo à aplicação",
+  });
 });
 
 app.use("/users", usersRouter);
